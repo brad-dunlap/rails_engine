742aee2a934d5b57447839a3c8e53e91a9e9eee4
Brad Dunlap <braddunlapdpt@gmail.com>
2023-03-30 11:45:44 -0600

    refactor to correct failing tests with previous refactor

--- app/controllers/api/v1/item/search_controller.rb
+++ app/controllers/api/v1/item/search_controller.rb
@@ -1,10 +1,12 @@
+
+
 class Api::V1::Item::SearchController < ApplicationController
 	def show
-		if params[:name] && (params[:min_price] || params[:max_price])
-			render json: { errors: "cannot send name with price"}, status: 400
-		elsif params[:name]
+		if (params[:name] && (params[:min_price] || params[:max_price]))
+			render json: ErrorSerializer.invalid_parameters("cannot send name with price"), status: 400
+		elsif (params[:name])
 			by_name
-		else params[:min] || params[:max]
+		else (params[:min] || params[:max])
 			by_price
 		end
 	end
@@ -13,19 +15,19 @@ class Api::V1::Item::SearchController < ApplicationController
 
 	def by_name
 		if Item.search_by_name(params[:name]).nil?
-			render json: { errors: "no results found"}, status: :not_found
+			render json: ErrorSerializer.invalid_parameters("no results found")
 		else
 			render json: ItemSerializer.new(Item.search_by_name(params[:name]))
 		end
 	end
 
 	def by_price
-		if params[:min_price].to_f < 0 || params[:max_price].to_f < 0
+		if (params[:min_price].to_f < 0) || (params[:max_price].to_f < 0)
 			render json: { errors: "price cannot be negative" }, status: 400
 		else
 			items = Item.search_by_price(params[:min_price], params[:max_price])
 			if items.nil?
-				render json: { errors: "no matches found" }, status: 400
+				render json: ErrorSerializer.no_matches_found("no matches found"), status: 400
 			else
 				render json: ItemSerializer.new(items)
 			end
