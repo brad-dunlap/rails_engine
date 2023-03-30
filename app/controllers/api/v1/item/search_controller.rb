b3a9586c8bc3afefeb97e0512215ca11da8177fe
Brad Dunlap <braddunlapdpt@gmail.com>
2023-03-30 11:37:50 -0600

    refactors

--- app/controllers/api/v1/item/search_controller.rb
+++ app/controllers/api/v1/item/search_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::Item::SearchController < ApplicationController
 	def show
-		if (params[:name] && (params[:min_price] || params[:max_price]))
+		if params[:name] && (params[:min_price] || params[:max_price])
 			render json: { errors: "cannot send name with price"}, status: 400
-		elsif (params[:name])
+		elsif params[:name]
 			by_name
-		else (params[:min] || params[:max])
+		else params[:min] || params[:max]
 			by_price
 		end
 	end
@@ -20,7 +20,7 @@ class Api::V1::Item::SearchController < ApplicationController
 	end
 
 	def by_price
-		if (params[:min_price].to_f < 0) || (params[:max_price].to_f < 0)
+		if params[:min_price].to_f < 0 || params[:max_price].to_f < 0
 			render json: { errors: "price cannot be negative" }, status: 400
 		else
 			items = Item.search_by_price(params[:min_price], params[:max_price])
